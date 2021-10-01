7790ed56809d5f461b136c641fb9eb64b0274f86
kandagatla9 <srinathkandagatla6@gmail.com>
2021-10-01 18:32:31 +0530

    responsess

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class FormComponent{
+  companyname:string;
+  requiredForm: FormGroup;
+  title = 'task2';
+  constructor(private fb: FormBuilder){
+    this.requiredForm = fb.group({
+      companyname: ['', [Validators.required, Validators.pattern("[a-zA-Z]{3,}")]],
+      companyEmailId: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
+      companywebsite: ['', [Validators.required, Validators.pattern("https?://.+")]],
+      aboutcompany: ['', [Validators.required, Validators.pattern("[a-zA-Z]{50,}")]],
+      contactno: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
+      industrytype: ['', [Validators.required, Validators.pattern("[a-zA-Z]{2,}")]],
+      country:  ['', [Validators.required, Validators.pattern("[a-zA-Z]{3,}")]],
+      state:  ['', [Validators.required, Validators.pattern("[a-zA-Z]{3,}")]],
+      city:  ['', [Validators.required, Validators.pattern("[a-zA-Z]{3,}")]],
+      companyaddress:  ['', [Validators.required, Validators.pattern("[a-zA-Z0-9]{4,}")]],
+      companyaddress2:  ['', [Validators.required, Validators.pattern("[a-zA-Z0-9]{4,}")]],
+      contactpersonname: ['', [Validators.required, Validators.pattern("[a-zA-Z]{3,}")]],
+     email2: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]]
+    })
   }
-
+  get m(){
+    return this.requiredForm.controls;
+  }
 }
